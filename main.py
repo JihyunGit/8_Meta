f842c5e9bed18f1f681796aa316352537ce21488
JihyunGit <hjhr95@gmail.com>
2022-08-26 16:24:49 +0900

    메인 주석달고 약간 수정함

--- main.py
+++ main.py
@@ -58,6 +58,7 @@ x_pos_list = [-3.06,-0.86,1.34,3.54]
 y_pos_list = [-0.82,0.43,1.68,2.92]
 
 # 카테고리
+# FlowerPot, PhotoFrame, BookShelf, Stand는 Color : None이고 Floor는 가구가 아님
 furlist = ['Bed', 'BookShelf', 'Chair', 'Desk', 'FlowerPot', 'PhotoFrame', 'Sofa', 'Stand', 'Floor']
 colorlist = ['Yellow', 'Blue', 'Green', 'White', 'Red', 'Brown', 'None']
 
@@ -131,7 +132,6 @@ def FromJsonToDB():
 
                 for relative in relative_list:
                     result = relativeDB.update({'Image':relative['Image']}, relative, upsert=True)
-                    print(result)
 
 # 이미 저장된 상품들의 관련 상품들 불러와서 DB에 저장
 def LoadDBProduct():
@@ -197,7 +197,7 @@ def index():
 # output : 사용자의 배치한 인테리어 배치도
 @app.route('/Login', methods=['POST'])
 def login():
-    print("login")
+    print("로그인")
     json_data = request.get_json()
     deviceId = json_data['DeviceId']
 
@@ -216,11 +216,14 @@ def login():
     else:
         return MakeResultJson(True, result['Map'])
 
-# 해당 아이디 없으면 insert되고 있으면 update하는 방식
-# 성공시 True 값 반환
+
+# 맵 저장
+# input : DeviceId, Map
+# output : 성공여부
+# 해당 아이디 없으면 insert되고 존재하면 update하는 방식
 @app.route('/Save', methods=['POST'])
 def Save():
-    print("save")
+    print("맵저장")
     json_data = request.get_json()
     deviceId = json_data['DeviceId']
     map = json_data['Map']
@@ -233,21 +236,21 @@ def Save():
     return MakeResultJson(True)
 
 
-# csv를 받아서 DB에 저장하고
-# 저장된 DB에서 색상, 카테고리에 속하는 아이템들 불러오고
-# 그 정보를 json형태로 리턴
+# 실제 가구 추천 (카테고리 / 색상 기반)
+# input : FurnitureType, ColorType
+# output : productDB에서 검색결과에 맞는 상품 리스트
 @app.route('/Recommend', methods=['POST'])
 def Recommend():
-    print("Recommend")
+    print("실제 가구 추천")
 
     json_data = request.get_json()
     RecommendRequestData = json_data['RecommendRequestData']
-    #Index = RecommendRequestData['Index']
+
     FurnitureType = RecommendRequestData['FurnitureType']
     ColorType = RecommendRequestData['ColorType']
 
-    # 테이블에서 검색해서 결과 가져오기
-    product_list = list(productDB.find({'Category':FurnitureType, 'Color':ColorType}))
+    # 테이블에서 검색해서 결과 가져오기, 최신순
+    product_list = list(productDB.find({'Category':FurnitureType, 'Color':ColorType}).sort("_id", -1))
 
     result_bool = False
 
@@ -264,9 +267,14 @@ def Recommend():
     return result
 
 
+# 가구별로 아무 제품이나 랜덤 추천
+# input : 가구 카테고리
+# output : productDB의 가구별 상품 하나씩만
 @app.route('/RecommendRandom', methods=['POST'])
 def RandomRecommend():
-    cate_list = ['Bed', 'BookShelf','Chair','Desk','FlowerPot','PhotoFrame','Sofa','Stand']
+    print('랜덤 상품 추천')
+    
+    cate_list = ['Bed','BookShelf','Chair','Desk','FlowerPot','PhotoFrame','Sofa','Stand']
 
     random_list = []
 
@@ -298,10 +306,10 @@ def RandomRecommend():
 # output : Result, pos_x,pos_y 추천 위치
 @app.route('/RecommendPos', methods=['POST'])
 def RecommendPosition():
+    print('추천 위치 반환')
+
     json_data = request.get_json()
     PosRequestData = json_data['PosRequestData']
-
-    print(PosRequestData)
 
     recom_list = recom_position_unity.load_best_area(PosRequestData['FurnitureType'], PosRequestData['ColorType'])
 
@@ -312,30 +320,20 @@ def RecommendPosition():
     if len(recom_list) > 0:
         result_bool = True
 
-    # list to json format
-    # json.dumps(리스트,)
-    # https://pythonexamples.org/python-list-to-json/
-    ## recom_json = dumps(recom_list)
-
     data_list = {"Result": result_bool, "Data": recom_list}
-    print(data_list)
 
     return data_list
 
 
-## Basket ------------------------------- 장바구니
+## Basket ------------------------------- 장바구니 -------------------------------
 
-# 장바구니에 추가
-# input : deviceId, Link컬럼의 id=에 있는 id 리스트만 저장
-# output : result_bool
-
-# 장바구니 업데이트
-# input : deviceId, id리스트
-# output : result_bool
-
-## 장바구니 Create and update
+## 장바구니 추가
+## input : DeviceId, productList
+## output : 성공여부
 @app.route('/UpdateBasket', methods=['POST'])
 def UpdateBasketDB():
+    print('장바구니 추가')
+
     json_data = request.get_json()
 
     deviceId = json_data['DeviceId']
@@ -351,17 +349,17 @@ def UpdateBasketDB():
 
     Data = {'Result':result_bool}
 
-    print(Data)
-
     return jsonify(Data)
 
 
 # 장바구니 불러오기
 # input : deviceId
-# 있으면 불러오기
-# output : id로 product테이블의 상품들 정보 찾아옴
+# output: 장바구니의 물품 리스트
+
 @app.route('/LoadBasket', methods=['POST'])
 def LoadBasketDB():
+    print('장바구니 로드')
+
     json_data = request.get_json()
     deviceId = json_data['DeviceId']
 
@@ -369,23 +367,25 @@ def LoadBasketDB():
 
     result = 'saaaaaa'
 
+    result_data = []
+
+    # 장바구니가 존재하면
     if (basket_list):
-
+        
         product_list_str = basket_list['ProductList']
-
+        # str형태를 다시 리스트로
         product_list = json.loads(product_list_str)
 
         print(product_list)
 
         tmp_str = '.*'
 
-        result_data = []
-
         result_bool = False
 
         if (len(product_list) > 0):
             result_bool = True
             for prod in product_list:
+                # productDB의 Link컬럼에서 *아이디 상품 찾아옴
                 product_info = productDB.find_one({'Link':{'$regex' : tmp_str+prod}})
                 result_data.append(product_info)
 
@@ -397,20 +397,22 @@ def LoadBasketDB():
     return result
 
 
-# 장바구니 삭제?
+# 장바구니 삭제
 # input : deviceId
-# output : result_bool
+# output : 성공여부
 @app.route('/DeleteBasket', methods=['POST'])
 def DeleteBasketDB():
+    print('장바구니 삭제')
+    
     json_data = request.get_json()
-
     deviceId = json_data['DeviceId']
 
+    # DB에서 삭제
     result = basketDB.delete_one({'DeviceId':deviceId})
 
     result_bool = False
-    # 삭제된 개수
-    print('삭제된 개수:',result.deleted_count)
+
+    print('삭제된 개수:', result.deleted_count)
 
     if (result.deleted_count > 0):
         result_bool = True
@@ -419,14 +421,16 @@ def DeleteBasketDB():
 
     return result_data
 
-## ------------------------------------장바구니 끝
+## ------------------------------------장바구니 끝 ----------------------------
 
-## 관련 상품 추천 Load
-## input : 상품의 상품명
-## Relative컬럼에 조건문 걸어서 일치하는 것 가져옴
+
+## 장바구니 상품과 관련된 상품 추천 로드하기
+## input : 상품명 (Title)
 ## output : RelativeDB의 일치하는 상품 리스트
 @app.route('/LoadRelative', methods=['POST'])
 def LoadRelativeDB():
+    print('장바구니와 관련된 상품 리스트 추천')
+
     json_data = request.get_json()
     relative_data = json_data['relativeRequestData']
 
@@ -450,11 +454,12 @@ def LoadRelativeDB():
     return result
 
 
-## 메타버스 내의 가구 타입과 색상에 따른
+## 메타버스 내의 가구 타입과 색상에 따른 추천
 ## input : 가구타입, 색상타입 (string)
 ## output : 추천 가구의 타입,색상조합 리스트와 코사인유사도
 @app.route('/MetaRecommendType', methods=['POST'])
 def MetaRecommendType():
+    print('메타버스 가구 추천')
     json_data = request.get_json()
     relative_data = json_data['MetaRecommendRequestData']
 
@@ -479,7 +484,7 @@ def MetaRecommendType():
         result_index = recom_df.index.to_list()
         result_value = recom_df.to_list()
         
-        # 리스트 2개를 dict로 묶기
+        # 리스트 2개를 dict로 묶기 (카테고리/색상과 코사인유사도 묶기)
         for index,value in zip(result_index, result_value):
             tmp_dict = {}
             tmp_dict['index'] = index
@@ -492,34 +497,42 @@ def MetaRecommendType():
 
     return data_list
 
+
 ## --------------------------- 중고거래 게시판 시작 --------------------------
 
-## 이미지 하나 가져오기 및 저장
+## 이미지 하나 가져오기 및 이미지 서버에 저장
 @app.route('/GetImageClass', methods=['POST'])
 def GetImageClass():
+    print('이미지 저장하기')
     img_file = request.files['file']
-
+    
+    # 로컬에 폴더 생성
     os.makedirs('./upload_images', exist_ok=True)
 
-    # 해킹 방지용 파일이름
-    img_file.save('./upload_images/' + secure_filename(img_file.filename))
-
-    img = open('./upload_images/' + secure_filename(img_file.filename), 'rb')
+    file_name = secure_filename(img_file.filename)
+    
+    # 로컬에 저장
+    img_file.save('./upload_images/' + file_name)
+    # FileStorage형태로 다시 부르기
+    img = open('./upload_images/' + file_name, 'rb')
 
     # 서버로 파일 저장
     SendFileToUrl(img)
 
-    print(secure_filename(img_file.filename))
+    print(file_name)
 
     return 'done!'
 
+
 # 이미지 분류 모델
 # input : 이미지 파일
-# output : 레이블, 어떤 가구인지
+# output : 어떤 가구인지 레이블
 @app.route('/ReturnFurnitureClass', methods=['POST'])
 def GetFurnitureClass():
+    print('이미지로 가구 카테고리 분류하기')
     img_file = request.files['file']
 
+    # 이미지 분류 모델로 예측한 카테고리 반환
     pred = realClassification.predict_img(img_file)
 
     print(pred)
@@ -530,10 +543,14 @@ def GetFurnitureClass():
 
 
 ## 게시판 글 등록
+# 카테고리, 가구명, 사진, 가격, 게시판 제목, 내용, 등록자id, 게시판 인덱스, 상태
+# input : form-data, 이미지파일과 게시글의 정보
+# output : 성공여부
+# 이미지 파일은 이미지 서버에 따로 저장하고 게시글 DB에 게시글 정보 저장
 @app.route('/InsertUsedBoard', methods=['POST'])
 def InsertUsed():
+    print('중고거래 게시판 글 등록 및 수정')
     # 이미지 파일 받기
-    ## request.files.getlist("file[]") -> 만약 리스트 형식이면 파라미터명은 file[]
     img_file = request.files['file']
 
     # 파일명
@@ -543,22 +560,21 @@ def InsertUsed():
 
     # form 데이터 받기
     form_data = request.form.to_dict()
-    
-    ## 글 정보 보냈으면
-    # 카테고리, 가구명, 사진, 가격, 게시판 제목, 내용, 등록자id, 게시판 인덱스
 
     # 인덱스는 숫자로 저장
     index = 1
 
     ## 여기는 글 수정
+    # 만약 인덱스를 보냈고 그 인덱스 값이 -1보다 크면 글 수정임
     if ('index' in form_data) and (int(form_data['index']) > -1):
-        ## 무조건 인덱스
-        print('수정')
+        print('글 수정')
         index = int(form_data['index'])
         form_data['index'] = index
     else:
-        ## 글 등록이면 여기로
-        print('등록')
+        ## 글 등록
+        print('글 등록')
+        # 게시글에서 최신 글 가져와서 인덱스 값 추출
+        # 글이 하나도 없으면 index는 1임
         results = list(usedDB.find({}).sort("_id", -1))
 
         ## 테이블에 데이터가 하나라도 있어야 조건에 걸림
@@ -570,21 +586,22 @@ def InsertUsed():
 
 
     # 이미지 파일명 (경로는 폴더명 변경에 유의해 넣지 않음)
-    # 그대로 넣으면 찾아오기 힘드니까 index + 확장자명 (1.jpg)
+    # 그대로 넣으면 찾아오기 힘드니까 index + 확장자명 (ex: 1.jpg)
     tmp_name = str(index) + '.' + extension
     form_data['imgName'] = tmp_name
+    
     # 0 : 정상, 1 : 삭제, 2 : 비공개
     form_data['state'] = 0
 
     print(form_data)
 
-    ## 이미지 저장할 폴더 없으면
+    ## 이미지 저장할 폴더 없으면 생성
     os.makedirs('./upload_images', exist_ok=True)
     
     ## 이미지 파일 저장
     img_file.save('./upload_images/' + tmp_name)
 
-    ## 다시 열기
+    ## 다시 열고
     img = open('./upload_images/' + tmp_name, 'rb')
     
     # 서버로 파일 저장
@@ -604,62 +621,14 @@ def InsertUsed():
 
     return result_data
 
-    
-## 게시판 글 수정
-## input : 인덱스 및 카테고리, 가구명, 사진, 가격, 게시판제목, 내용
-## output : 결과값
-# @app.route('/UpdateUsedBoard', methods=['POST'])
-# def UpdatetUsed():
-#     # 이미지 파일 받고
-#     img_file = request.files['file']
-#
-#     # 파일명
-#     file_name = secure_filename(img_file.filename)
-#
-#     # 확장자
-#     extension = file_name.split('.')[-1]
-#
-#     # form 데이터 받기
-#     form_data = request.form.to_dict()
-#
-#     # 수정할 글의 인덱스
-#     index = form_data['index']
-#
-#     # 저장할 폴더, 없으면 생성
-#     os.makedirs('./upload_images', exist_ok=True)
-#
-#     # 파일명
-#     tmp_name = str(index) + '.' + extension
-#     # 파일명 변경
-#     form_data['imgName'] = tmp_name
-#
-#     # DB 테이블에 업데이트s
-#     result = usedDB.update({'index':index}, form_data, upsert=True)
-#
-#     print(result)
-#
-#     result_bool = False
-#
-#     if (len(result) > 0):
-#         ## 이미지 파일 저장
-#         img_file.save('./upload_images/' + tmp_name)
-#         # 서버로 파일 저장
-#         img_file.filename = tmp_name
-#         SendFileToUrl(img_file)
-#
-#         result_bool = True
-#
-#     result_data = {'result_bool':result_bool}
-#
-#     return result_data
-
 
 ## 중고거래 게시판 로드하기
 ## input : 없음
 ## output : 게시판 글
 @app.route('/LoadUsed', methods=['POST'])
 def LoadUsedDB():
-    # state가 0인 정상적인 게시글들만 가져오기
+    print('중고거래 게시판 로드하기')
+    # state가 0인 정상적인 게시글들만 최신순으로 가져오기
     board_list = list(usedDB.find({'state':0}).sort("_id", -1))
 
     print(board_list)
@@ -672,19 +641,22 @@ def LoadUsedDB():
 
         print(len(board_list))
 
-    data_list = {"Result":result_bool,"Data":board_list}
+    data_list = {"Result":result_bool, "Data":board_list}
 
     # bson -> json 형태로
     result = dumps(data_list, ensure_ascii=False)
 
     return result
 
+
 # 중고거래 게시글 삭제 (실제 삭제는 아님, state를 변경해서 state가 0인 목록만 정상임)
 # state : 0(정상), 1(삭제), 2(비공개)
-# input : 인덱스
+# input : 게시글 인덱스
 # output : 성공여부
 @app.route('/DeleteUsed', methods=['POST'])
 def DeleteUsedDB():
+    print('중고거래 게시판 삭제')
+
     json_data = request.get_json()
     # 삭제할 게시글의 인덱스
     index = int(json_data['index'])
@@ -716,9 +688,6 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
 
-
-
-
 # 스케쥴 설정
 #sched = BackgroundScheduler(daemon=True)
 # sched_result = sched.add_job(Craw.makeJsonItem, 'cron', week='1-4', day_of_week='0-6', minute='2', 'interval')
@@ -742,8 +711,6 @@ if __name__ == '__main__':
     # serve(app, host="0.0.0.0", port=5000)
     # app.run(debug=True) # host = 127.0.0.1 port = 5000
     app.run(host='0.0.0.0', port='5080', debug=False)
-
-# debug=True이므로 코드 수정 중에도 알아서 다시 시작해줌
 
 
 
